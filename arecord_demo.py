2da32d14a61800001f8c228c3b5904475abfcd50
Dedward5 <david@zennor.co.uk>
2017-03-15 22:03:19 +0000

    more hack stuff

--- arecord_demo.py
+++ arecord_demo.py
@@ -4,13 +4,17 @@ import  subprocess, os, sys
 import threading
 import time
 
-cmd1= "arecord -D plughw:1 --duration=10 -f cd -vv /media/usb/recorded5.wav"
+cmd1= "arecord -D plughw:1 -f cd -vv /media/usb/recorded7.wav"
 pro1= subprocess.Popen("exec " + cmd1,stdout=subprocess.PIPE, shell=True)
 
 
 
 print("Audio record is only for 10sec")
-time.sleep(5)
+
+for i in range (25):
+	time.sleep(1)
+	print (i)
+
 print("I quit")
 
 pro1.kill()
